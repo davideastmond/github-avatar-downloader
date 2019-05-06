f2bc0e0a3b9fb8d1b21277f2fd858866920154d5
David Eastmond <davebuck1981@gmail.com>
2019-05-06 17:02:10 -0400

    enabled input from command line

--- download_avatars.js
+++ download_avatars.js
@@ -1,1 +1,10 @@
 var request = require('request');
+
+// Pull the repoOwner and repoName from the command line
+var clRepoOwner = process.argv[2];
+var clRepoName = process.argv[3];
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+  console.log(repoOwner, " owns ", repoName);
+}
+getRepoContributors(clRepoOwner, clRepoName);
