98259524fe6fb08fb77879ae72783f15a29a6d71
David Eastmond <davebuck1981@gmail.com>
2019-05-06 17:44:03 -0400

    implemented tokens / secrets

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,38 @@
 var request = require('request');
+var testSite = "https://api.github.com/repos/jquery/jquery/contributors"
+
 
 // Pull the repoOwner and repoName from the command line
 var clRepoOwner = process.argv[2];
 var clRepoName = process.argv[3];
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
-  console.log(repoOwner, " owns ", repoName);
+  // Ensure that the owner and repo_name fields are populated
+  if (!repoOwner || !repoName) {
+    // Undefined parameters, exit the function
+    console.log("ERROR: Please specify a repo owner and name");
+    return;
+  }
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/" + "contributors",
+    headers: {
+      'User-Agent': 'davideastmond'
+    }
+  };
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+  
 }
-getRepoContributors(clRepoOwner, clRepoName);
+
+// Get the data
+function displayData(error, data) {
+  if (error) {
+    console.log("ERROR ", error);
+    return;
+  }
+
+  console.log(data);
+}
+// Sample
+getRepoContributors("jquery", "jquery", displayData);
