ca8f49df87e4c431daab3ea481aa3babe6f342ed
David Eastmond <davebuck1981@gmail.com>
2019-05-06 19:16:47 -0400

    completed functionality

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var fs = require("fs");
 var testSite = "https://api.github.com/repos/jquery/jquery/contributors"
 
 
@@ -19,6 +20,9 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'User-Agent': 'davideastmond'
     }
   };
+
+  /* Create a request to get the repo owner and contributors JSON for further processing in the call
+  back function */
   request(options, function(err, res, body) {
     cb(err, body);
   });
@@ -32,13 +36,41 @@ function displayData(error, data) {
     return;
   }
 
-  // Get the JSON
+  // Get the JSON and parse it into a js object
   var parsedData = JSON.parse(data);
   
   // Print the URL of each image
   for (var i = 0; i < parsedData.length; i++) {
-    console.log(parsedData[i].avatar_url);
+    // Create a local file path using the user's login name
+     var filePath = "./avatars/" + parsedData[i].login + ".jpg";
+     var URLData = parsedData[i].avatar_url;
+
+     // As we iterate through the JSON object, issue requests to fetch the image data and store it on disk
+     downloadImageByURL(URLData, filePath, parsedData[i].login); // TEST
   }
 }
-// Sample
-getRepoContributors("jquery", "jquery", displayData);
+
+function downloadImageByURL(URL, finalPath, username, callback) {
+  // Retrieve actual images and store them
+  request.get(URL)
+    .on ('error', (err)=> {
+      // If there was an error, print the message to the console
+      console.log("There was an error. ", err);
+    })
+
+    .on('response', (response)=> {
+      // Read headers etc.
+    })
+
+    .on('end', ()=> {
+      // The download has finished, console log for the completion of each user's downloaded image
+      console.log("Image downloaded for user: ", username);
+    })
+
+    // Using the pipe, immediately write the stream to local file
+    .pipe(fs.createWriteStream(finalPath));
+}
+
+
+// Call the function with process.argv parameters
+getRepoContributors(clRepoOwner, clRepoName, displayData);
